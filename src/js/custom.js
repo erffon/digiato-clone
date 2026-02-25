be0d47961d8a556af34568c9bb97f0ab1a8e5448
erffon <erffon@gmail.com>
2026-02-25 13:30:03 +0330

    Add: Mobile carousel

---
+++ src/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".hero-section__carousel").slick({
+    rtl: true,
+    centerMode: false,
+    slidesToShow: 1.2,
+    infinite: false,
+    arrows: false,
+    dots: false,
+    focusOnSelect: true,
+  });
+});
